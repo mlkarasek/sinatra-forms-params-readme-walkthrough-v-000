9b088d2cf19be46599502514791dff0977dd7d73
Monica  Karasek <mlkarasek@gmail.com>
2018-08-11 09:58:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,10 +6,6 @@ class App < Sinatra::Base
     erb :food_form
   end
 
-  get '/food' do
-    erb :food
-  end
-
   post '/food' do
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
